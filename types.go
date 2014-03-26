6a9d28e1751aac20229e7e2543a4585ac8c308c5
Luan Santos <cfcluan@gmail.com>
2014-03-25 23:32:25 -0700

    Print Z coordinate with positions

--- types.go
+++ types.go
@@ -8,9 +8,9 @@ type Goal struct{ Position }
 type Player struct{ Position }
 
 func (p Player) String() string {
-	return fmt.Sprintf("Player{%d,%d}", p.X, p.Y)
+	return fmt.Sprintf("Player{%d,%d,%d}", p.X, p.Y, p.Z)
 }
 
 func (g Goal) String() string {
-	return fmt.Sprintf("Goal{%d,%d}", g.X, g.Y)
+	return fmt.Sprintf("Goal{%d,%d,%d}", g.X, g.Y, g.Z)
 }
